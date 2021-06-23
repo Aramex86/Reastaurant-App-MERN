ccdf467c483696ae30b7d332e18bfd932fe129f5
Aramex86 <slavbez@mail.ru>
2021-06-23 16:23:13 +0300

    change server path

--- server/server.js
+++ server/server.js
@@ -47,12 +47,18 @@ app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
 //production!
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(port, () => {
   console.log(`listen on port ${port}`);
 });
