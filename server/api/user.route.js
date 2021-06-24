7b95e65168310a4b99f148763f7b901c3d3e8fde
Aramex86 <slavbez@mail.ru>
2021-06-24 15:55:01 +0300

    change static path

--- server/api/user.route.js
+++ server/api/user.route.js
@@ -11,7 +11,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = express.Router();
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, "../upload"));
--- server/server.js
+++ server/server.js
@@ -52,10 +52,9 @@ app.use("/api/v1/profileBg", background);
 // });
 
 if (process.env.NODE_ENV === "production") {
-  // app.use(express.static("../client/build"));
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
+  app.use(express.static("../client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 app.listen(port, () => {
