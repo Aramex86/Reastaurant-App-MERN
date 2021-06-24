63cdfa4e3c2bc6c94e1970315216f3ac877a9f52
Aramex86 <slavbez@mail.ru>
2021-06-24 17:13:41 +0300

    modify files

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "upload")));
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 const url = process.env.DATABASE;
 mongoose
