d120b12c3246e0befcb38cca0d25aaafee802d5f
Aramex86 <slavbez@mail.ru>
2021-06-27 20:17:29 +0300

    change api client

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -13,7 +13,7 @@ export const MY_API = "AIzaSyBhr_PmeQMzMCoERPzs3cfNNzuJ89Ld_Ss";
 
 // axios.defaults.withCredentials = true;
 const instance = axios.create({
-  baseURL: `http://localhost:5000/api/v1/`,
+  baseURL: `/api/v1/`,
   withCredentials: true,
 });
 
